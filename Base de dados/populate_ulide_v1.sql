043ca211205191c78eaf8412ef57b7cb299ed0c5
Leonerdo15 <leonardolage10@gmail.com>
2021-11-12 07:50:49 +0000

    multiple pk erro

--- Base de dados/populate_ulide_v1.sql
+++ Base de dados/populate_ulide_v1.sql
@@ -1,5 +1,21 @@
+insert into tag_types (tt_name) values ('User');
+insert into tag_types (tt_name) values ('System');
 
--------------------------------------------------------- users ---------------------------------------------------------
+insert into tags (tg_name, tg_tt_id) values ('Mercado', 1);
+insert into tags (tg_name, tg_tt_id) values ('Monumento', 1);
+insert into tags (tg_name, tg_tt_id) values ('Natureza', 1);
+insert into tags (tg_name, tg_tt_id) values ('Museu', 1);
+insert into tags (tg_name, tg_tt_id) values ('Desporto', 1);
+insert into tags (tg_name, tg_tt_id) values ('Festa', 2);
+insert into tags (tg_name, tg_tt_id) values ('Cool', 2);
+insert into tags (tg_name, tg_tt_id) values ('Sexta-feira', 2);
+insert into tags (tg_name, tg_tt_id) values ('Mediaval', 1);
+insert into tags (tg_name, tg_tt_id) values ('Ar livre', 2);
+insert into tags (tg_name, tg_tt_id) values ('Conhecimento',1);
+insert into tags (tg_name, tg_tt_id) values ('Elavdor',1);
+insert into tags (tg_name, tg_tt_id) values ('Estádio', 1);
+insert into tags (tg_name, tg_tt_id) values ('Criança', 1);
+
 
 insert into type_users(tu_name) values ('Super');
 insert into type_users(tu_name) values ('Nivel 1');
@@ -29,27 +45,59 @@ insert into users(us_name, us_bdate, us_gender)
 values ('Plácido Ramires', to_date('1992-07-17', 'YYYY-MM-DD'), 'M');
 insert into users(us_name, us_bdate, us_gender)
 values ('Nicolau Esteves', to_date('2001-10-01', 'YYYY-MM-DD'), 'M');
----------------------------------------------------- end of users ------------------------------------------------------
+
+-- true is paid, false is free
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Torre de Belém', 38.69175116131192, -9.215966573567318, false);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Mosteiro dos Jerónimos', 38.69806671988026, -9.206671715894556, false);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Castelo de São Jorge', 38.71405986610344, -9.133454744730386, true);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Padrão dos Descobrimentos', 38.6936015,-9.2079002, false);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Museu da Cidade', 38.7585930361228, -9.156325382521535, true);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Elevador da Glória', 38.716897389128455, -9.142660122889284, true);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Estádio da Luz', 38.752886685013436, -9.184763273565512, true);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Oceanário', 38.76371079269964, -9.093720044728894, true);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Praça do Comércio', 38.70759494492502, -9.13644380826236, false);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Jardim Zoológico', 38.744495366088586, -9.170685015893174, true);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Panteão Nacional', 38.715061350403744, -9.12490880796462, false);
+insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Mercado da Ribeira', 38.70690732582062, -9.145907544730553, false);
+insert into spots (sp_name, sp_lat, sp_long) values('Mercado do Príncipe Real', 38.716159660346214, -9.149212773566605);
+insert into spots (sp_name, sp_lat, sp_long) values('Mercado de Campo de Ourique', 38.707173108334246, -9.143361173566847);
+insert into spots (sp_name, sp_lat, sp_long) values('Lx Factory', 38.70366533341345, -9.178862273566953);
+insert into spots (sp_name, sp_lat, sp_long) values('Miradouro de Santa Catarina', 38.709725975239834, -9.147623365894209);
 
 
--------------------------------------------- Everything related with tags ----------------------------------------------
-insert into tag_types (tt_name) values ('User');
-insert into tag_types (tt_name) values ('System');
+insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
+values (5, 'Fantastic', 2, 12);
+insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
+values (4, 'Mui bueno', 2, 13);
+insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
+values (1, 'Not for me', 2, 14);
+insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
+values (5, 'Im a tea pot', 2, 15);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (4, 2, 8);
 
-insert into tags (tg_name, tg_tt_id) values ('Mercado', 1);
-insert into tags (tg_name, tg_tt_id) values ('Monumento', 1);
-insert into tags (tg_name, tg_tt_id) values ('Natureza', 1);
-insert into tags (tg_name, tg_tt_id) values ('Museu', 1);
-insert into tags (tg_name, tg_tt_id) values ('Desporto', 1);
-insert into tags (tg_name, tg_tt_id) values ('Festa', 2);
-insert into tags (tg_name, tg_tt_id) values ('Cool', 2);
-insert into tags (tg_name, tg_tt_id) values ('Sexta-feira', 2);
-insert into tags (tg_name, tg_tt_id) values ('Mediaval', 1);
-insert into tags (tg_name, tg_tt_id) values ('Ar livre', 2);
-insert into tags (tg_name, tg_tt_id) values ('Conhecimento',1);
-insert into tags (tg_name, tg_tt_id) values ('Elavdor',1);
-insert into tags (tg_name, tg_tt_id) values ('Estádio', 1);
-insert into tags (tg_name, tg_tt_id) values ('Criança', 1);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (5, 3, 1);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (4, 3, 2);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (2, 3, 5);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (3, 3, 11);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (4, 3, 16);
+
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (5, 4, 5);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (4, 4, 2);
+
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (3, 5, 8);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (4, 5, 16);
+insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
+values (5, 5, 6);
 
 insert into users_spots_tags(ust_sp_id, ust_tg_id, ust_us_id) values (1, 2, 1);
 insert into users_spots_tags(ust_sp_id, ust_tg_id, ust_us_id) values (1, 7, 3);
@@ -101,29 +149,6 @@ insert into users_spots_tags(ust_sp_id, ust_tg_id, ust_us_id) values (14, 1, 1);
 insert into users_spots_tags(ust_sp_id, ust_tg_id, ust_us_id) values (15, 1, 1);
 insert into users_spots_tags(ust_sp_id, ust_tg_id, ust_us_id) values (15, 10, 3);
 
-------------------------------------------------- end tags -------------------------------------------------------------
-
-
-
---------------------------------------- Everything related with spots --------------------------------------------------
--- true is paid, false is free
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Torre de Belém', 38.69175116131192, -9.215966573567318, false);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Mosteiro dos Jerónimos', 38.69806671988026, -9.206671715894556, false);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Castelo de São Jorge', 38.71405986610344, -9.133454744730386, true);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Padrão dos Descobrimentos', 38.6936015,-9.2079002, false);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Museu da Cidade', 38.7585930361228, -9.156325382521535, true);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Elevador da Glória', 38.716897389128455, -9.142660122889284, true);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Estádio da Luz', 38.752886685013436, -9.184763273565512, true);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Oceanário', 38.76371079269964, -9.093720044728894, true);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Praça do Comércio', 38.70759494492502, -9.13644380826236, false);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Jardim Zoológico', 38.744495366088586, -9.170685015893174, true);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Panteão Nacional', 38.715061350403744, -9.12490880796462, false);
-insert into spots (sp_name, sp_lat, sp_long, sp_price) values('Mercado da Ribeira', 38.70690732582062, -9.145907544730553, false);
-insert into spots (sp_name, sp_lat, sp_long) values('Mercado do Príncipe Real', 38.716159660346214, -9.149212773566605);
-insert into spots (sp_name, sp_lat, sp_long) values('Mercado de Campo de Ourique', 38.707173108334246, -9.143361173566847);
-insert into spots (sp_name, sp_lat, sp_long) values('Lx Factory', 38.70366533341345, -9.178862273566953);
-insert into spots (sp_name, sp_lat, sp_long) values('Miradouro de Santa Catarina', 38.709725975239834, -9.147623365894209);
-
 
 insert into fav_spots(fs_us_id, fs_sp_id) values (2, 5);
 insert into fav_spots(fs_us_id, fs_sp_id) values (2, 6);
@@ -155,41 +180,6 @@ insert into fav_spots(fs_us_id, fs_sp_id) values (7, 6);
 insert into fav_spots(fs_us_id, fs_sp_id) values (7, 13);
 insert into fav_spots(fs_us_id, fs_sp_id) values (7, 16);
 
-
-insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
-values (5, 'Fantastic', 2, 12);
-insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
-values (4, 'Mui bueno', 2, 13);
-insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
-values (1, 'Not for me', 2, 14);
-insert into spot_evaluations(se_rate, se_comment, se_us_id, se_sp_id)
-values (5, 'Im a tea pot', 2, 15);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (4, 2, 8);
-
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (5, 3, 1);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (4, 3, 2);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (2, 3, 5);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (3, 3, 11);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (4, 3, 16);
-
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (5, 4, 5);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (4, 4, 2);
-
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (3, 5, 8);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (4, 5, 16);
-insert into spot_evaluations(se_rate, se_us_id, se_sp_id)
-values (5, 5, 6);
-
 insert into done_spots(ds_us_id, ds_sp_id) values (2, 12);
 insert into done_spots(ds_us_id, ds_sp_id) values (2, 13);
 insert into done_spots(ds_us_id, ds_sp_id) values (2, 14);
@@ -208,9 +198,49 @@ insert into done_spots(ds_us_id, ds_sp_id) values (4, 2);
 insert into done_spots(ds_us_id, ds_sp_id) values (5, 8);
 insert into done_spots(ds_us_id, ds_sp_id) values (5, 16);
 insert into done_spots(ds_us_id, ds_sp_id) values (5, 6);
------------------------------------------------------ End o spots ------------------------------------------------------
 
--------------------------------------------- Everything related with routes --------------------------------------------
+insert into routes (rt_name,rt_bio, rt_dist) values ('Compras', 'Capitalismo no seu melhor', 4);
+insert into routes (rt_name, rt_bio, rt_dist) values ('Natureza', 'Muitos animais agua e plantas, brother!!!', 10);
+insert into routes (rt_name,rt_dist) values ('Histórica', 3);
+insert into routes (rt_name,rt_dist) values ('Popular', 2);
+insert into routes (rt_name,rt_dist) values ('Gratis', 6);
+
+insert into fav_routes(fr_us_id, fr_rt_id) values (2, 1);
+insert into fav_routes(fr_us_id, fr_rt_id) values (3, 1);
+insert into fav_routes(fr_us_id, fr_rt_id) values (4, 1);
+insert into fav_routes(fr_us_id, fr_rt_id) values (5, 1);
+insert into fav_routes(fr_us_id, fr_rt_id) values (9, 1);
+insert into fav_routes(fr_us_id, fr_rt_id) values (2, 1);
+
+insert into fav_routes(fr_us_id, fr_rt_id) values (3, 3);
+insert into fav_routes(fr_us_id, fr_rt_id) values (4, 3);
+insert into fav_routes(fr_us_id, fr_rt_id) values (6, 3);
+
+insert into fav_routes(fr_us_id, fr_rt_id) values (2, 4);
+insert into fav_routes(fr_us_id, fr_rt_id) values (8, 4);
+insert into fav_routes(fr_us_id, fr_rt_id) values (9, 4);
+
+insert into fav_routes(fr_us_id, fr_rt_id) values (5, 5);
+
+
+insert into route_evaluations(re_rate, re_comment, re_us_id, re_rt_id)
+values (5, 'Very good!', 2, 1);
+insert into route_evaluations(re_rate, re_us_id, re_rt_id)
+values (3, 3, 3);
+insert into route_evaluations(re_rate, re_us_id, re_rt_id)
+values (2, 4, 4);
+insert into route_evaluations(re_rate, re_us_id, re_rt_id)
+values (4, 5, 2);
+
+
+insert into done_routes(dr_us_id, dr_rt_id) values (2, 1);
+insert into done_routes(dr_us_id, dr_rt_id) values (3, 3);
+insert into done_routes(dr_us_id, dr_rt_id) values (4, 4);
+insert into done_routes(dr_us_id, dr_rt_id) values (5, 2);
+
+insert into progress_routes(pr_us_id, pr_rt_id, pr_sp_id) values (2, 2, 16);
+
+
 insert into route_spots (rs_rt_id, rs_sp_id) values (1, 12);
 insert into route_spots (rs_rt_id, rs_sp_id) values (1, 13);
 insert into route_spots (rs_rt_id, rs_sp_id) values (1, 14);
@@ -237,52 +267,6 @@ insert into route_spots (rs_rt_id, rs_sp_id) values (5, 9);
 insert into route_spots (rs_rt_id, rs_sp_id) values (5, 11);
 insert into route_spots (rs_rt_id, rs_sp_id) values (5, 12);
 
-
-insert into routes (rt_name,rt_bio, rt_dist) values ('Compras', 'Capitalismo no seu melhor', 4);
-insert into routes (rt_name, rt_bio, rt_dist) values ('Natureza', 'Muitos animais agua e plantas, brother!!!', 10);
-insert into routes (rt_name,rt_dist) values ('Histórica', 3);
-insert into routes (rt_name,rt_dist) values ('Popular', 2);
-insert into routes (rt_name,rt_dist) values ('Gratis', 6);
-
-
-insert into route_evaluations(re_rate, re_comment, re_us_id, re_rt_id)
-values (5, 'Very good!', 2, 1);
-insert into route_evaluations(re_rate, re_us_id, re_rt_id)
-values (3, 3, 3);
-insert into route_evaluations(re_rate, re_us_id, re_rt_id)
-values (2, 4, 4);
-insert into route_evaluations(re_rate, re_us_id, re_rt_id)
-values (4, 5, 2);
-
-
-
-insert into done_routes(dr_us_id, dr_rt_id) values (2, 1);
-insert into done_routes(dr_us_id, dr_rt_id) values (3, 3);
-insert into done_routes(dr_us_id, dr_rt_id) values (4, 4);
-insert into done_routes(dr_us_id, dr_rt_id) values (5, 2);
-
-insert into progress_routes(pr_us_id, pr_rt_id, pr_sp_id) values (2, 2, 16);
-
-insert into fav_routes(fr_us_id, fr_rt_id) values (2, 1);
-insert into fav_routes(fr_us_id, fr_rt_id) values (3, 1);
-insert into fav_routes(fr_us_id, fr_rt_id) values (4, 1);
-insert into fav_routes(fr_us_id, fr_rt_id) values (5, 1);
-insert into fav_routes(fr_us_id, fr_rt_id) values (9, 1);
-insert into fav_routes(fr_us_id, fr_rt_id) values (12, 1);
-
-insert into fav_routes(fr_us_id, fr_rt_id) values (3, 3);
-insert into fav_routes(fr_us_id, fr_rt_id) values (4, 3);
-insert into fav_routes(fr_us_id, fr_rt_id) values (6, 3);
-
-insert into fav_routes(fr_us_id, fr_rt_id) values (2, 4);
-insert into fav_routes(fr_us_id, fr_rt_id) values (14, 4);
-insert into fav_routes(fr_us_id, fr_rt_id) values (15, 4);
-
-insert into fav_routes(fr_us_id, fr_rt_id) values (5, 5);
-
------------------------------------------ End Routes -------------------------------------------------------------------
-
------------------------------- Everything related with achievements ----------------------------------------------------
 insert into achievements(ac_name) values ('1 Museu Visitado');
 insert into achievements(ac_name) values ('5 Museus Visitados');
 insert into achievements(ac_name) values ('10 Museus Visitados');
@@ -317,4 +301,3 @@ insert into user_achievements (ua_date, ua_us_id, ua_ac_id) values (to_date('202
 insert into user_achievements (ua_date, ua_us_id, ua_ac_id) values (to_date('2021-11-19', 'YYYY-MM-DD'), 3, 13);
 
 insert into user_achievements (ua_date, ua_us_id, ua_ac_id) values (to_date('2021-11-11', 'YYYY-MM-DD'), 4, 1);
--------------------------------------- End achievements ----------------------------------------------------------------
