75b588b590f51c155443553b2a077dd630f752f7
Leonerdo15 <leonardolage10@gmail.com>
2021-11-29 16:54:15 +0000

    spots maps in lisbon

--- Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/MyProfileFragment.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/MyProfileFragment.java
@@ -1,33 +1,18 @@
 package com.example.ulide.ui.myProfile;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.ulide.R;
 import com.example.ulide.databinding.FragmentMyProfileBinding;
-import com.example.ulide.downloaders.JSONArrayDownloader;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 public class MyProfileFragment extends Fragment {
 
@@ -36,6 +21,7 @@ public class MyProfileFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
 
 
         binding = FragmentMyProfileBinding.inflate(inflater, container, false);
--- Android/Ulide/app/src/main/java/com/example/ulide/ui/spotsFromRoute/SpotsFromRouteFragment.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/spotsFromRoute/SpotsFromRouteFragment.java
@@ -9,15 +9,23 @@ import androidx.fragment.app.FragmentResultListener;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.ulide.MainActivity;
+import com.example.ulide.R;
 import com.example.ulide.databinding.FragmentSpotsFromRouteBinding;
 import com.example.ulide.downloaders.JSONArrayDownloader;
 import com.example.ulide.ui.findRoutes.FindRoutesFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -40,11 +48,27 @@ public class SpotsFromRouteFragment extends Fragment {
         // Required empty public constructor
     }
 
+//    @Override
+//    public void onPrepareOptionsMenu(@NonNull Menu menu) {
+//        super.onPrepareOptionsMenu(menu);
+//        MenuItem item = menu.findItem(R.id.spots);
+//        item.setVisible(isEditing);
+//    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentSpotsFromRouteBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+
+        setHasOptionsMenu(false);
+
 
         listViewSpots = binding.listViewSpots;
 
@@ -65,6 +89,31 @@ public class SpotsFromRouteFragment extends Fragment {
 
         return root;
     }
+
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        private GoogleMap mMap;
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+            // Add a marker in Sydney and move the camera
+            LatLng lisbon = new LatLng(38.736946, -9.142685);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lisbon, 13));
+        }
+    };
 
     public void getJSON(String id, String name){
         JSONArrayDownloader task = new JSONArrayDownloader();
