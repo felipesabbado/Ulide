cf4800a208531c31a4fce273139a21876abaea8d
Felipe Campelo Sabbado <fsabbado@gmail.com>
2021-11-29 18:48:07 +0000

    Creat mark points in spots from routes

--- Android/Ulide/app/src/main/java/com/example/ulide/ui/spotsFromRoute/SpotsFromRouteFragment.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/spotsFromRoute/SpotsFromRouteFragment.java
@@ -1,6 +1,5 @@
 package com.example.ulide.ui.spotsFromRoute;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,10 +8,9 @@ import androidx.fragment.app.FragmentResultListener;
 
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,12 +18,13 @@ import com.example.ulide.MainActivity;
 import com.example.ulide.R;
 import com.example.ulide.databinding.FragmentSpotsFromRouteBinding;
 import com.example.ulide.downloaders.JSONArrayDownloader;
-import com.example.ulide.ui.findRoutes.FindRoutesFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -42,7 +41,11 @@ public class SpotsFromRouteFragment extends Fragment {
     private ArrayAdapter<String> adapterSpots;
     private ArrayList<String> spots;
     private ArrayList<String> spotsId;
+    private ArrayList<String> lat;
+    private ArrayList<String> lng;
     private JSONArray spotsArray;
+    private GoogleMap mMap;
+    private LatLng spotPos;
 
     public SpotsFromRouteFragment() {
         // Required empty public constructor
@@ -69,11 +72,11 @@ public class SpotsFromRouteFragment extends Fragment {
 
         setHasOptionsMenu(false);
 
-
         listViewSpots = binding.listViewSpots;
 
 
-        getParentFragmentManager().setFragmentResultListener("route", this, new FragmentResultListener() {
+        getParentFragmentManager().setFragmentResultListener("route",
+                this, new FragmentResultListener() {
             @Override
             public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle bundle) {
                 // We use a String here, but any type that can be put in a Bundle is supported
@@ -82,38 +85,12 @@ public class SpotsFromRouteFragment extends Fragment {
                 // Do something with the result
                 ((MainActivity) getActivity()).getSupportActionBar().setTitle(name);
 
-
                 getJSON(id,name);
             }
         });
 
         return root;
     }
-
-    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
-
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        private GoogleMap mMap;
-
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            mMap = googleMap;
-
-            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-
-            // Add a marker in Sydney and move the camera
-            LatLng lisbon = new LatLng(38.736946, -9.142685);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lisbon, 13));
-        }
-    };
 
     public void getJSON(String id, String name){
         JSONArrayDownloader task = new JSONArrayDownloader();
@@ -128,12 +105,16 @@ public class SpotsFromRouteFragment extends Fragment {
         JSONObject obj;
         spots = new ArrayList<>();
         spotsId = new ArrayList<>();
+        lat = new ArrayList<>();
+        lng = new ArrayList<>();
         if (spotsArray != null) {
             for (int i = 0; i < spotsArray.length(); i++) {
                 try {
                     obj = spotsArray.getJSONObject(i);
                     spots.add(obj.getString("spName"));
                     spotsId.add(obj.getString("id"));
+                    lat.add(obj.getString("spLat"));
+                    lng.add(obj.getString("spLong"));
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -147,5 +128,44 @@ public class SpotsFromRouteFragment extends Fragment {
     public void InitalizeAdapter() {
         adapterSpots = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, spots);
         listViewSpots.setAdapter(adapterSpots);
+        createListViewClickItemEvent(listViewSpots, spots);
+    }
+
+    private final OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mMap = googleMap;
+
+            mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+            // Add a marker in each spot
+            for(int i = 0; i < spotsArray.length(); i++){
+                spotPos = new LatLng(Double.parseDouble(lat.get(i)), Double.parseDouble(lng.get(i)));
+                mMap.addMarker(new MarkerOptions().position(spotPos).title(spots.get(i)));
+            }
+            LatLng lisbon = new LatLng(38.736946, -9.142685);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lisbon, 11));
+        }
+    };
+
+    private void createListViewClickItemEvent(ListView list, final ArrayList<String> item) {
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                spotPos = new LatLng(Double.parseDouble(lat.get(i)), Double.parseDouble(lng.get(i)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(spotPos, 13.5f));
+                for (int k = 0; k < spotsArray.length(); k++){
+                    if (k != i){
+                        spotPos = new LatLng(Double.parseDouble(lat.get(k)), Double.parseDouble(lng.get(k)));
+                        mMap.addMarker(new MarkerOptions().position(spotPos)
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+                    } else {
+                        spotPos = new LatLng(Double.parseDouble(lat.get(i)), Double.parseDouble(lng.get(i)));
+                        mMap.addMarker(new MarkerOptions().position(spotPos)
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                    }
+                }
+            }
+        });
     }
 }
