6307d2ceeae327b3632dfed38a6654f0a4b6e008
Leonerdo15 <leonardolage10@gmail.com>
2021-12-06 18:09:18 +0000

    sign up gender

--- Android/Ulide/app/src/main/java/com/example/ulide/MainActivity.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
 //        username = binding.username;
 
+        username = findViewById(R.id.username);
+
         setSupportActionBar(binding.appBarMain.toolbar);
 
         DrawerLayout drawer = binding.drawerLayout;
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        username.setText("guessed");
         return true;
     }
 
--- Android/Ulide/app/src/main/java/com/example/ulide/ui/signUp/SignUpActivity.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/signUp/SignUpActivity.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.ulide.R;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText bDate;
     DatePickerDialog.OnDateSetListener setListener;
+    Spinner gender;
+    ArrayAdapter<String> adapterGender;
+    ArrayList<String> listGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,20 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         bDate = findViewById(R.id.editTextBDate);
+        gender = findViewById(R.id.spinnerGender);
+
+        listGender = new ArrayList<>();
+
+        listGender.add("Gender");
+        listGender.add("Male");
+        listGender.add("Female");
+        listGender.add("Binary");
+
+
+        adapterGender = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listGender);
+        adapterGender.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gender.setAdapter(adapterGender);
+
 
 
         Calendar calendar = Calendar.getInstance();
