4005eb3572e200644571b8da5c9efa1ce2aa3a8e
Felipe Campelo Sabbado <fsabbado@gmail.com>
2021-12-06 20:17:22 +0000

    Update location mananger on CreatSpots

--- Android/Ulide/app/src/main/java/com/example/ulide/ui/createSpots/CreateSpotsFragment.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/createSpots/CreateSpotsFragment.java
@@ -1,10 +1,20 @@
 package com.example.ulide.ui.createSpots;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +35,10 @@ public class CreateSpotsFragment extends Fragment {
     private FragmentCreateSpotsBinding binding;
     private JSONArray spot = null;
     private EditText spotName, latitude, longitude;
-    private Button submit;
+    private Button submit, myLocation;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private String lat, lng;
 
     public CreateSpotsFragment() {
         // Required empty public constructor
@@ -34,6 +47,68 @@ public class CreateSpotsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // 1 - Criar o location Manager para ir buscar a localização onde o nosso device está
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // 2 - Criar um location Listener para detetar mudanças na nossa localização
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                lat = String.valueOf(location.getLatitude());
+                lng = String.valueOf(location.getLongitude());
+                String msg="New Latitude: "+ lat + " New Longitude: "+ lng;
+                Log.e("Location", msg);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        // 3 - Detectar se o utilizador nos deu permissões para obter a localização
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        } else {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    5000,
+                    0,
+                    locationListener
+            );
+        }
+    }
+
+    // 4 - Verificar se temos permissões ao executar a nossa Activity
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        0,
+                        0,
+                        locationListener
+                );
+            }
+        }
     }
 
     @Override
@@ -47,6 +122,7 @@ public class CreateSpotsFragment extends Fragment {
         latitude = binding.editTextLatitude;
         longitude = binding.editTextLongitude;
         submit = binding.buttonSubmit;
+        myLocation = binding.buttonMyLocation;
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +156,14 @@ public class CreateSpotsFragment extends Fragment {
                     e.printStackTrace();
                     spot = null;
                 }
+            }
+        });
+
+        myLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                latitude.setText(lat);
+                longitude.setText(lng);
             }
         });
 
