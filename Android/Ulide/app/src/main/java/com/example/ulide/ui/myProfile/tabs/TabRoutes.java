3268a9b410ddfa23901abe51a69e66847d2add18
Felipe Campelo Sabbado <fsabbado@gmail.com>
2021-12-08 20:27:22 +0000

    Create tab Fragments

--- Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/tabs/TabRoutes.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/tabs/TabRoutes.java
@@ -1,11 +1,24 @@
 package com.example.ulide.ui.myProfile.tabs;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class TabSpots extends Fragment {
-    public TabSpots() {
+import com.example.ulide.databinding.FragmentTabRoutesBinding;
 
+public class TabRoutes extends Fragment {
+
+    private FragmentTabRoutesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentTabRoutesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
     }
-
-
 }
--- Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/tabs/TabSpots.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/tabs/TabSpots.java
@@ -1,2 +1,23 @@
-package com.example.ulide.ui.myProfile.tabs;public class TabSpots {
+package com.example.ulide.ui.myProfile.tabs;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.ulide.databinding.FragmentTabSpotsBinding;
+
+public class TabSpots extends Fragment {
+
+    private FragmentTabSpotsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentTabSpotsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        return root;
+    }
 }
