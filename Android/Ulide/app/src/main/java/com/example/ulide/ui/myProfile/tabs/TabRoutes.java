10f0dbfb3b85f81b6354dd8c6a619535398642fa
Felipe Campelo Sabbado <fsabbado@gmail.com>
2021-12-12 19:28:00 +0000

    Create Tabs Routes

--- Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/tabs/TabRoutes.java
+++ Android/Ulide/app/src/main/java/com/example/ulide/ui/myProfile/tabs/TabRoutes.java
@@ -8,17 +8,35 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.ulide.databinding.FragmentTabRoutesBinding;
+import com.example.ulide.ui.myProfile.MyAdapter;
+import com.google.android.material.tabs.TabLayout;
 
 public class TabRoutes extends Fragment {
 
     private FragmentTabRoutesBinding binding;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentTabRoutesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        tabLayout = binding.tabRoutes;
+        viewPager = binding.viewerPageRoutes;
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        MyAdapter adapter = new MyAdapter(getActivity().getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        adapter.addFragment(new TabRoutesDone(), "Done");
+        adapter.addFragment(new TabRoutesFav(), "Favorites");
+        adapter.addFragment(new TabRoutesEval(), "Evaluations");
+        viewPager.setAdapter(adapter);
+
         return root;
     }
 }
